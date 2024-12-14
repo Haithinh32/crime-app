5ca01b6b81b22a3110d9912b953fc459c596b79f
Haithinh32 <119341079+Haithinh32@users.noreply.github.com>
2024-12-14 22:02:22 +0700

    make view

---
+++ resources/views/Post.blade.php
@@ -0,0 +1,45 @@
+<x-guest-layout>
+    <x-slot:header>
+        <div class="w-[599px] h-[53px] relative bg-gradient-to-b from-[#b21c1c] via-[#b32121] to-white rounded-[20px]">
+            <div class="w-[599px] h-px left-0 top-[52px] absolute bg-white"></div>
+            <div class="left-[16px] top-[14px] absolute justify-start items-start gap-2.5 inline-flex">
+                <div class="text-[#f5faff] text-xl font-bold font-['Inter']">Bài đăng</div>
+            </div>
+            <div class="w-[34px] h-[34px] left-[549px] top-[9px] absolute rounded-[34px]">
+                <div class="w-5 h-5 left-[7px] top-[7px] absolute"></div>
+            </div>
+        </div>
+    </x-slot:header>
+    <div class="w-[475px] h-[717px] relative">
+        <div class="w-[381.99px] h-[31.83px] left-[37px] top-[487px] absolute">
+            <div class="w-[381.99px] h-[31.83px] left-0 top-0 absolute bg-white rounded-[10px] border border-black"></div>
+            <div class="w-[121px] h-6 left-[228px] top-[4px] absolute">
+                <div class="w-[121px] h-6 left-0 top-0 absolute bg-[#d9d9d9] rounded-[30px]"></div>
+                <div class="left-[28px] top-[3px] absolute text-[#161616] text-sm font-bold font-['Inter']">Đăng bài</div>
+            </div>
+            <div class="w-5 h-5 left-[9px] top-[6px] absolute"></div>
+            <div class="w-5 h-5 left-[43px] top-[6px] absolute"></div>
+        </div>
+        <div class="w-[382px] h-[200px] left-[36.70px] top-[275px] absolute">
+            <div class="w-[119px] h-[19px] left-[19px] top-0 absolute text-[#161616] text-[15px] font-bold font-['Inter']">Nội dung</div>
+            <div class="w-[382px] h-[166px] left-0 top-[34px] absolute">
+                <div class="w-[382px] h-[166px] left-0 top-0 absolute bg-white rounded-[10px] border border-black"></div>
+                <div class="w-[167px] h-[17px] left-[19.30px] top-[10px] absolute text-[#d9d9d9] text-sm font-normal font-['Inter']">Nhập vào đây</div>
+            </div>
+        </div>
+        <div class="w-[381.99px] h-[62.65px] left-[37px] top-[197.18px] absolute">
+            <div class="w-[148.21px] h-[15.92px] left-[18.35px] top-0 absolute text-[#161616] text-[15px] font-bold font-['Inter']">Loại tội phạm</div>
+            <div class="w-[381.99px] h-[31.83px] left-0 top-[30.82px] absolute">
+                <div class="w-[381.99px] h-[31.83px] left-0 top-0 absolute bg-white rounded-[10px] border border-black"></div>
+                <div class="w-[167px] h-[17px] left-[18px] top-[7px] absolute text-[#d9d9d9] text-sm font-normal font-['Inter']">Nhập vào đây</div>
+            </div>
+        </div>
+        <div class="w-[381.99px] h-[65.78px] left-[37.01px] top-[119.72px] absolute">
+            <div class="w-[175.71px] h-[19.10px] left-[18.34px] top-0 absolute text-[#161616] text-[15px] font-bold font-['Inter']">Tiêu đề</div>
+            <div class="w-[381.99px] h-[31.83px] left-0 top-[33.95px] absolute">
+                <div class="w-[381.99px] h-[31.83px] left-0 top-0 absolute bg-white rounded-[10px] border border-black"></div>
+                <div class="w-[167px] h-[17px] left-[18.99px] top-[7.33px] absolute text-[#d9d9d9] text-sm font-normal font-['Inter']">Nhập vào đây</div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center">
+        <div class="w-[1920px] h-[1080px] relative bg-white overflow-y-hidden">
+            <div class="w-[276px] h-[1080px] left-[327px] top-0 absolute sticky; top: 0;">
+                <div class="w-[276px] h-[1080px] left-0 top-0 absolute bg-gradient-to-t from-[#f9b115] via-[#d15519] to-red-700">
+                @include('components.sidebar_admin')
+                </div>
+            </div>
+            <div class="w-[600px] h-[1491px] left-[618px] top-[4px] absolute">
+                <div class="w-[600px] h-[1491px] left-0 top-0 absolute bg-white rounded-[20px] shadow">
+                    <div class="w-[599px] h-[53px] left-0 top-0 absolute bg-gradient-to-b from-[#b21c1c] via-[#b32121] to-white rounded-[20px]">
+                      {{ $header }}
+                    </div>
+                    <div class="w-[599px] h-px left-0 top-[52px] absolute bg-white"></div>
+                        {{ $slot }}
+                </div>
+            </div>
+            <div class="w-[360px] h-[1080px] left-[1233px] top-0 absolute">
+                <div class="w-[360px] h-[1080px] left-0 top-0 absolute bg-gradient-to-t from-[#f9b115] via-[#d25619] to-red-700"></div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
